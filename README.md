acfd939179ba9f1601ec405dc71464ef4837e599
Jedd Saliba <jeddsaliba@gmail.com>
2022-02-20 22:34:43 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# laravel-qbo" 
+QBO_AUTH_MODE=
+QBO_ATH_REQUEST_URL=
+QBO_TOKEN_END_POINT_URL=
+QBO_CLIENT_ID=
+QBO_CLIENT_SECRET=
+QBO_REDIRECT_URI=
+QBO_SCOPE=
+QBO_BASE_URL=
+QBO_COMPANY_ID=
+
+php artisan vendor:publish --provider="Pns\LaravelQbo\Providers\LaravelQboServiceProvider" --tag="config"
+
+php artisan vendor:publish --provider="Pns\LaravelQbo\Providers\LaravelQboServiceProvider" --tag="migrations"
--- src/config/config.php
+++ src/config/config.php
@@ -1,5 +1,15 @@
 <?php
+
 return [
-    'hello' => 'world'
+    'auth_mode' => env('QBO_AUTH_MODE', NULL),
+    'request_url' => env('QBO_ATH_REQUEST_URL', NULL),
+    'end_point_url' => env('QBO_TOKEN_END_POINT_URL', NULL),
+    'client_id' => env('QBO_CLIENT_ID', NULL),
+    'client_secret' => env('QBO_CLIENT_SECRET', NULL),
+    'redirect_uri' => env('QBO_REDIRECT_URI', NULL),
+    'scope' => env('QBO_SCOPE', NULL),
+    'base_url' => env('QBO_BASE_URL', NULL),
+    'company_id' => env('QBO_COMPANY_ID', NULL)
 ];
+
 ?>
